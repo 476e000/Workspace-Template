ec0ad8bcb163b3a872a48cd2199c396d5a452925
476e000 <44920283+476e000@users.noreply.github.com>
2026-03-10 13:52:09 +0800

    refactor

--- cpp-workspace/qt/include/MainWindow.h
+++ cpp-workspace/qt/include/MainWindow.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #include <QMainWindow>
+#include <memory>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
@@ -9,6 +14,28 @@ public:
 	MainWindow(QWidget* parent = nullptr);
 	~MainWindow();
 
-private:
+protected:
+	void changeEvent(QEvent* evt) override;
 
+private:
+	std::unique_ptr<Ui::MainWindow> ui;
+};
+
+
+// ------ translator ------ //
+
+class Translator
+{
+	struct Data;
+public:
+	~Translator();
+	static Translator& instance();
+
+	bool load(const QLocale& locale);
+
+private:
+	Translator();
+
+private:
+	std::unique_ptr<Data> d;
 };
