8a1f823d59144e15248ea81b05a193e79963b570
476e000 <44920283+476e000@users.noreply.github.com>
2026-05-09 12:04:13 +0800

    add C# workspace templates

---
+++ cs-workspace/net472/#cs#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace #cs#;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        return 0;
+    }
+}
